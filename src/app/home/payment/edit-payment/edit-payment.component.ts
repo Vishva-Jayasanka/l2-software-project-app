db14e2ddeb07ac7755b9137ef146e5425b2042b4
onali <onalisharindi@gmail.com>
2021-03-21 07:58:49 +0530

    feature payment frontend changes

--- src/app/home/payment/edit-payment/edit-payment.component.ts
+++ src/app/home/payment/edit-payment/edit-payment.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {EMPTY, Subject, Subscription} from 'rxjs';
+import {DataService} from '../../../_services/data.service';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
+import {Bank} from '../upload-payment/upload-payment.component';
 
 @Component({
   selector: 'app-edit-payment',
@@ -6,10 +11,131 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-payment.component.css']
 })
 export class EditPaymentComponent implements OnInit {
+  editPaymentProgress = false;
+  studentIDNotFound = false;
+  success = false;
 
-  constructor() { }
+  error = '';
 
-  ngOnInit(): void {
+  banks: Bank[] = [
+    {bankID: 1, bankName: 'BOC'},
+    {bankID: 2, bankName: 'Peoples Bank'}
+  ];
+
+  paymentForm: FormGroup;
+  term$ = new Subject<string>();
+  private searchSubscription: Subscription;
+  private elementRef: ElementRef;
+  buttonProgress: false;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private data: DataService
+  ) {
+
+    this.searchSubscription = this.term$.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap(studentID => {
+        this.error = '';
+        this.success = false;
+        this.studentIDNotFound = false;
+        this.checkStudentID(studentID);
+        return EMPTY;
+      })
+    ).subscribe();
   }
 
+  ngOnInit(): void {
+    this.paymentForm = this.formBuilder.group({
+        depositor: this.formBuilder.group({
+          registrationNumber: ['', [Validators.required, Validators.pattern(/^([0-9]{6}[A-Za-z])$/)]],
+          fullName: [''],
+        }),
+        deposit: this.formBuilder.group({
+          bankName: ['', [Validators.required]],
+          slipNumber: ['', [Validators.required]],
+          totalPaid: ['', [Validators.required]],
+          paymentDate: ['', [Validators.required]],
+        }),
+      }
+    );
+  }
+
+  submitForm() {
+    this.data.uploadPayment(this.paymentForm.value).subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        this.error = error;
+      }
+    );
+  }
+
+  checkStudentID(studentID) {
+    this.success = false;
+    this.error = '';
+    this.studentIDNotFound = false;
+    if (studentID) {
+      this.data.checkStudentID(studentID).subscribe(
+        response => {
+          if (response.status) {
+            this.fullName.setValue(response.name);
+          } else {
+            this.studentIDNotFound = true;
+          }
+        },
+        error => this.error = error
+      ).add(() => this.editPaymentProgress = false);
+    } else {
+      this.editPaymentProgress = false;
+    }
+  }
+
+  toggleProgress() {
+    this.editPaymentProgress = true;
+  }
+
+  get fullName() {
+    return this.paymentForm.get('depositor').get('fullName');
+  }
+
+  get registrationNumber() {
+    return this.paymentForm.get('depositor').get('registrationNumber');
+  }
+
+  get bankName() {
+    return this.paymentForm.get('deposit').get('bankName');
+  }
+
+  get slipNumber() {
+    return this.paymentForm.get('deposit').get('slipNumber');
+  }
+
+  get totalPaid() {
+    return this.paymentForm.get('deposit').get('totalPaid');
+  }
+
+  get paymentDate() {
+    return this.paymentForm.get('deposit').get('totalPaid');
+  }
+
+  resetForm() {
+    this.paymentForm.reset();
+    this.elementRef.nativeElement.querySelector('#course-name').scrollIntoView();
+  }
+
+  getPayments() {
+
+  }
+
+  deletePayments() {
+
+  }
+
+  modifyPayments() {
+
+  }
 }
