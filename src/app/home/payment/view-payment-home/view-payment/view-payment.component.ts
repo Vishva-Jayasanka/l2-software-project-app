ab0ac92318023a281cf7af5166673695c937e829
Onalie <onalisharindi@gmail.com>
2021-03-26 00:46:41 +0530

    Registration changes

---
+++ src/app/home/payment/view-payment-home/view-payment/view-payment.component.ts
@@ -0,0 +1,53 @@
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {DataService} from '../../../_services/data.service';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
+import {EMPTY, Subject, Subscription} from 'rxjs';
+import {YEARS} from '../../../_services/shared.service';
+
+export interface Course {
+  courseID: number;
+  courseName: string;
+}
+
+export const COURSES: Course[] = [
+  {courseID: 1, courseName: 'MSC/PG DIPLOMA IN INFORMATION TECHNOLOGY'},
+  {courseID: 2, courseName: 'MSC/PG DIPLOMA IN MULTIMEDIA TECHNOLOGY'},
+];
+
+@Component({
+  selector: 'app-view-payment',
+  templateUrl: './view-payment.component.html',
+  styleUrls: ['./view-payment.component.css']
+})
+export class ViewPaymentComponent implements OnInit {
+  viewPaymentsForm: FormGroup;
+  viewPaymentsProgress: boolean;
+  studentIDNotFound = false;
+  courses: Course[] = COURSES;
+  years = YEARS;
+  success = false;
+
+  error = '';
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private data: DataService,
+    private elementRef: ElementRef
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.viewPaymentsForm = this.formBuilder.group({
+      courseName: [1, [Validators.required]],
+      academicYear: ['', [Validators.required]]
+    });
+
+  }
+
+  get courseName() {
+    return this.viewPaymentsForm.get('courseName');
+  }
+
+}
