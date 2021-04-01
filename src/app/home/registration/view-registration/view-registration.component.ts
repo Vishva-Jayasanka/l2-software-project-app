564611838c5c8e0988b6baa5ea0213decb4a4eaa
Onalie <onalisharindi@gmail.com>
2021-04-01 22:07:52 +0530

    view registered users

--- src/app/home/registration/view-registration/view-registration.component.ts
+++ src/app/home/registration/view-registration/view-registration.component.ts
@@ -23,39 +23,11 @@ export interface PeriodicElement {
   position: number;
   studentID: string;
   title: string;
-  name: string;
+  fullName: string;
   nic: string;
   email: string;
   mobile: number;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-
-  {position: 1, studentID: '184025M',
-    title: 'Mr', name: 'Sadun Alwis', nic: '986040495V', email: 'sadunalwis@gmail.com', mobile: +94711240735},
-  {position: 2, studentID: '184025M',
-    title: 'Ms', name: 'Vishwa Jaysanka Atapattu', nic: '981251721V', email: 'vishwajayasanka@gmail.com', mobile: +94774253323},
-  {position: 3, studentID: '184025M',
-    title: 'Ms', name: 'Naduni Thakshila Bandara', nic: '976121321V', email: 'nadunibandara@gmail.com', mobile: +94773103834},
-  {position: 4, studentID: '184025M',
-    title: 'Ms', name: 'Tharushi Weerasingha', nic: '984242405V', email: 'tharushiweerasingha@gmail.com', mobile: +94778112974},
-  {position: 5, studentID: '184025M',
-    title: 'Mr', name: 'Kavidu Yasith Katuwandeniya', nic: '977700783V', email: 'kaviduyasith@gmail.com', mobile: +94714228357},
-  {position: 6, studentID: '184025M',
-    title: 'Mrs', name: 'Saduni Perera', nic: '981140404V', email: 'saduniperera@gmail.com', mobile: +94719114287},
-  {position: 7, studentID: '184025M',
-    title: 'Mr', name: 'Ravidu Shamika Kulathunga', nic: '982244405V', email: 'ravidukulath@gmail.com', mobile: +94777893550},
-  {position: 8, studentID: '184025M',
-    title: 'Ms', name: 'Onali Vithanage', nic: '995240424V', email: 'onalisharindi@gmail.com', mobile: +94714240853},
-  {position: 9, studentID: '184025M',
-    title: 'Ms', name: 'Nethmi Bimsara Jayasekara', nic: '976548122V', email: 'nethmibimsara@gmail.com', mobile: +94784965273},
-  {position: 10, studentID: '184025M',
-    title: 'Mrs', name: 'Thilini Wijekoon', nic: '971100407V', email: 'thiliniwijekoon@gmail.com', mobile: +94717477578},
-  {position: 11, studentID: '184025M',
-    title: 'Mr', name: 'Vihanaga Godakubura', nic: '975645340V', email: 'vihangagodakubura@gmail.com', mobile: +94777710280},
-  {position: 12, studentID: '184025M',
-    title: 'Mr', name: 'Chathura Perera', nic: '981254880V', email: 'chathuraperera@gmail.com', mobile: +94776651844},
-];
 
 @Component({
   selector: 'app-view-registration',
@@ -64,7 +36,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ViewRegistrationComponent implements OnInit, AfterViewInit {
   displayedColumns = ['position', 'studentID', 'title', 'name', 'nic', 'email', 'mobile', 'customDataColumn'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource;
   filterValue = '';
   viewRegistrationsForm: FormGroup;
   viewRegistrationProgress = false;
@@ -105,8 +77,6 @@ export class ViewRegistrationComponent implements OnInit, AfterViewInit {
 
   toggle() {
     this.show = !this.show;
-    this.filterValue = '';
-    this.dataSource.filter = '';
 
     if (this.show) {
       this.buttonName = 'Hide';
@@ -125,8 +95,10 @@ export class ViewRegistrationComponent implements OnInit, AfterViewInit {
   getData(){
     this.viewRegistrationProgress = true;
     this.data.getRegisteredUsers().subscribe(response => {
-          console.log(response);
-          this.fullName.setValue(response.name);
+          this.dataSource = new MatTableDataSource(response.results[0]);
+          this.filterValue = '';
+          this.dataSource.filter = '';
+          this.viewRegistrationProgress = false;
     });
   }
 
